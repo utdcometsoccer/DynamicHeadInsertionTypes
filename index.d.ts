b8fd4bc02563fbe77c529d7b8960b97b1b01231e
Idaho Edokpayi <idaho@edokpayi.com>
2019-02-16 23:15:11 -0500

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+// Type definitions for DynamicHeadInsertion
+// Project: DynamicHeadInsertion
+// Definitions by: Idaho Edokpayi <https://github.com/utdcometsoccer/>
+
+export = DynamicHeadInsertion;
+
+/*~ This example shows how to have multiple overloads for your function */
+declare function DynamicHeadInsertion(headElement: Element): void;
